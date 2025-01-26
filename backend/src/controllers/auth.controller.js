b4ca44f3d7b3cd0ce30cb4b7da1d333e1efcdb88
Samyak Jain <samyaksk10@gmail.com>
2025-01-27 00:45:02 +0530

    done with signup, login andlogout functionalities

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,11 +1,111 @@
-export const signupController = (req, res) => {
-    res.send('signup Route');
+import User from '../models/user.model.js';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import { generateToken } from '../lib/utils.js';
+
+
+export const signupController = async(req, res) => {
+       const {fullName, email, password} = req.body;
+    try{
+
+        if(!fullName || !email || !password){
+            return res.status(400).json({message: "All fields are required"});
+        }
+
+        if(password.length < 6){
+            return res.status(400).json({message: "Password should be atleast 6 characters long"});
+        }
+       
+        const user = await User.findOne({email});
+
+        if(user){
+            return res.status(400).json({message: "User already exists with this email"});
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = new User({fullName, email, password: hashedPassword});
+
+
+        if(newUser){
+          // generate jwt token
+          generateToken(newUser._id, res);
+
+          await newUser.save();
+          
+          return res.status(201).json({
+            _id: newUser._id,
+            fullName: newUser.fullName,
+            email: newUser.email,
+            profilePic: newUser.profilePic,
+            message: "User registered successfully"});
+        }
+        else{
+            return res.status(400).json({message: "Failed to register user"});
+        }
+
+    }
+    catch(error){
+        console.log("error in signup controller :" + error);
+    }
 };
 
-export const loginController = (req, res) => {     
-    res.send('Login Route');
+
+
+
+export const loginController =async(req, res) => {     
+  const { email, password } = req.body;
+    try {
+      if (!email || !password) {
+        return res.status(400).json({ message: "All fields are required" });
+      }
+
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({ message: "Invalid credentials" });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(400).json({ message: "Invalid credentials" });
+      }
+
+      // generate jwt token
+      generateToken(user._id, res);
+
+      return res.status(200).json({
+        _id: user._id,
+        fullName: user.fullName,
+        email: user.email,
+        profilePic: user.profilePic,
+        message: "User logged in successfully"
+      });
+      
+
+    } catch (error) {
+      res.send({
+        status: 500,
+        message: "Error in login controller",
+        error: error.message,
+      })
+      console.log("error in login controller :" + error);
+
+    }
 };  
 
-export const logoutController = (req, res) => {                     
-  res.send('Logout Route'); 
+export const logoutController = (req, res) => {          
+  try {
+    res.cookie('jwt', '', {maxAge: 0});
+  res.json({message: "User logged out successfully"});
+    
+  } catch (error) {
+    res.send({
+      status: 500,
+      message: "Error in logout controller",
+      error: error.message,
+    })
+    
+  }           
+  
 };
--- backend/src/index.js
+++ backend/src/index.js
@@ -7,6 +7,10 @@ import { connectDB } from "./lib/db.js";
 dotenv.config();
 
 
+// to extract json data from the body of the request
+app.use(express.json());
+
+
 app.use("/api/auth", authRoutes);
 
 const port = process.env.PORT || 5001;
