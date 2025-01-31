acf337bbcab459032cdb1024b550fdcf9f4dd7b3
Samyak Jain <samyaksk10@gmail.com>
2025-02-01 01:10:23 +0530

    done with the messages routing

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,4 +1,4 @@
-// import Message from "../models/message.model.js";
+import Message from "../models/message.model.js";
 import User from "../models/user.model.js";
 
 export const getUsersforSidebar = async (req, res) => {
@@ -12,4 +12,62 @@ export const getUsersforSidebar = async (req, res) => {
         res.status(500).json({ message: "Internal server error" });
         
     }
+};
+
+
+export const getMessages = async (req, res) => {
+    try {
+        const {id : userToChatId} = req.params;
+        const myId = req.user._id;
+
+        const messages = await Message.find({
+            $or: [
+                { senderId: myId, receiverId: userToChatId },
+                { senderId: userToChatId, receiverId: myId },
+            ],
+        }).sort({ createdAt: -1 });
+        
+        res.status(200).json(messages);
+
+    } catch (error) {
+        console.log("Error in getting messages "+error);
+        res.status(500).json({ message: "Internal server error" });
+        
+    }
+};
+
+
+export const sendMessage = async (req, res) => {
+    try {
+        const {id : receiverId} = req.params;
+        const senderId = req.user._id;
+        const { text,image } = req.body;
+        
+        let imageURL;
+    
+        // if user is sending image, then upload the image to cloudinary
+        if(image)
+        {
+            const uploadResponse = await cloudinary.uploader.upload(image);
+            imageURL = uploadResponse.secure_url;
+        }
+        
+        const newMessage = new Message({
+            senderId,
+            receiverId,
+            text,
+            image: imageURL,
+        });
+    
+    
+        await newMessage.save();
+        
+        res.status(201).json(newMessage);
+    
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error" });
+        console.log("Error in sending message "+error);
+    }
+   
+
 };
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,10 +1,16 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getUsersforSidebar } from "../controllers/message.controller.js";
+import { getMessages, getUsersforSidebar, sendMessage } from "../controllers/message.controller.js";
 
 const router = express.Router();
 
 // get all users for sidebar
 router.get("/users", protectRoute, getUsersforSidebar);
+
+// get messages from the receiver
+router.get("/:id",protectRoute, getMessages);
+
+// sending message to the receiver
+router.post("/send/:id",protectRoute, sendMessage);
 
 export default router;
