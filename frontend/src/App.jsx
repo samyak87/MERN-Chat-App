14a960b1641563b0fa179f133ebfdebf4affaf5e
Samyak Jain <samyaksk10@gmail.com>
2025-02-01 17:04:53 +0530

    started with routing of pages in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,27 @@
-import './App.css'
-function App() {
+import Navbar from './components/Navbar.jsx';
+import { Routes, Route } from "react-router-dom";
+import HomePage from './pages/HomePage.jsx';
+import SettingsPage from './pages/SettingsPage.jsx';
+import LoginPage from './pages/LoginPage.jsx';
+import SignupPage from './pages/SignupPage.jsx';
+import ProfilePage from './pages/ProfilePage.jsx';
 
+function App() {
   return (
     <>
-    <div className='text-red-500'>hii</div>
+     <div>
+       <Navbar/>
+       <Routes>
+         <Route path="/" element={<HomePage />} />
+         <Route path="/signup" element={<SignupPage />} />
+         <Route path="/login" element={<LoginPage />} />
+         <Route path="/settings" element={<SettingsPage />} />
+         <Route path="/profile" element={<ProfilePage />} />
+        </Routes>
+      </div>
+      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
